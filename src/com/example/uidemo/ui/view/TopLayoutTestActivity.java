225ab9c01c1fdb2bba331d634bedca5c9b2df5c3
whz <sohozhou@163.com>
2014-03-07 17:07:54 +0800

    scroll2bottom ok

--- src/com/example/uidemo/ui/view/TopLayoutTestActivity.java
+++ src/com/example/uidemo/ui/view/TopLayoutTestActivity.java
@@ -6,9 +6,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TopLayoutTestActivity extends Activity{
 	TopLayout mTopLayout;
+	Button mBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,18 @@ public class TopLayoutTestActivity extends Activity{
 	    setContentView(R.layout.toplayout_test);
 	    
 	    mTopLayout = (TopLayout) findViewById(R.id.toplayout);
+	    mBtn = (Button) findViewById(R.id.btn);
+	    mBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				 mTopLayout.scroll2Bottom();
+			}
+		});
 	    
 	    mTopLayout.scroll2Top();
+	    
+	   
 	}
 	
 }
